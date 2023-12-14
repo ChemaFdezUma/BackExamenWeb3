bab368750eea744b2e89d0b1b83c23f2fc689779
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-15 00:13:06 +0100

    Cloud

--- routes/paradasRoutes.js
+++ routes/paradasRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const axios = require("axios");
 const paradasSchema = require("../models/paradas.js");
 
 //get all
@@ -53,6 +53,39 @@ router.put("/:id", (req, res) => {
     .then((data) => res.json(data))
     .catch((error) => res.json({ message: error }));
 });
+
+// get todos las paradas en la linea x a sentido y
+router.get("/linea/:codLinea/sentido/:sentido", (req, res) => {
+  paradasSchema
+    .find({ codLinea: req.params.codLinea, sentido: req.params.sentido })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// get paradas por parte de nombre
+router.get("/nombre/:nombreParada", (req, res) => {
+  paradasSchema
+    .find({ nombreParada: { $regex: req.params.nombreParada } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//get paradas que esten a menos de 0,003 grados de longitud y latitud de la direccion X
+router.get("/direccion/:direccion", async (req, res) => {
+    const { direccion } = req.params;
+    console.log(direccion)
+        const respuesta = await axios.get(`https://nominatim.openstreetmap.org/search?q=${direccion}&format=json&limit=1`).then((response) => {
+        const lat = response.data[0].lat
+          const lon = response.data[0].lon
+          paradasSchema.find({ lat: { $gte: lat - 0.003, $lte: lat + 0.003 }, lon: { $gte: lon - 0.003, $lte: lon + 0.003 } })
+              .then((data) => res.json(data))
+              .catch((error) => res.json({ message: error }));
+        }).catch((error) => { console.log(error) });
+       
+
+    }
+
+);
 
 module.exports = router;
 
