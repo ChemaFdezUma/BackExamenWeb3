ff7f6fc9bebf00cd585409389b1bb9158188cc2c
ChemaFdezUma <100839162+ChemaFdezUma@users.noreply.github.com>
2023-12-15 01:50:34 +0100

    POR DIOS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const mongoose = require("mongoose")
 const multer = require('multer');
-const fileUpload = multer({ dest: 'uploads/' });
+const fileUpload = multer();
 const cloudinary = require('cloudinary');
 
 const app = express();
